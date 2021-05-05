c460183a826ffc3c6d4e584b23ec006914499706
David Hellner <danoobve@web.de>
2021-05-05 15:38:50 +0200

    Add Bottlegraphics + Colorchange + Sizechange
    Added Files for Bottlegraphics
    Added Logic for Colorchange
    Added Logic for changing the Size of the Bottles

--- src/components/Layout/Body/screens/ConfiguratorScreen/BierKonfigurator/Configuration/ConfigurationBottle/index.tsx
+++ src/components/Layout/Body/screens/ConfiguratorScreen/BierKonfigurator/Configuration/ConfigurationBottle/index.tsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import bottleGreenNormal from './bottle-green.png'
+import bottleWhiteNormal from './bottle-white.png'
+import bottleBrownNormal from './bottle-brown.png'
+import bottleBrownSmall from './bottle-brown-small.png'
+import bottleWhiteSmall from './bottle-white-small.png'
+import bottleGreenSmall from './bottle-green-small.png'
+
+import classes from './ConfigurationBottle.module.css'
 
 const ConfigurationBottle = () => {
-    return (
-        <h2>Configuration-Bottle</h2>
-    )
-};
+
+        // [brown, green, white]
+
+        const [bottle, setBottle] = useState({
+            sizeIsNormal: true,
+            color: 'brown',
+            usedGraphic: bottleBrownNormal,
+        })
+
+        const [headings, setHeadings] = useState({
+            colorChange: 'Change Color'
+        })
+
+
+        const bottleColorChangeHandlerRight = () => {
+            switch (bottle.color) {
+                case 'green': {
+                    let result = {...bottle, color: 'white', usedGraphic: bottleWhiteSmall}
+                    if (result.sizeIsNormal) result.usedGraphic = bottleWhiteNormal
+                    return setBottle(result);
+                }
+                case 'brown' : {
+                    let result = {...bottle, color: 'green', usedGraphic: bottleGreenSmall}
+                    if (result.sizeIsNormal) result.usedGraphic = bottleGreenNormal
+                    return setBottle(result);
+                }
+                case 'white' : {
+                    let result = {...bottle, color: 'brown', usedGraphic: bottleBrownSmall}
+                    if (result.sizeIsNormal) result.usedGraphic = bottleBrownNormal
+                    return setBottle(result);
+                }
+            }
+        }
+
+        const bottleChangeHandlerLeft = () => {
+            switch (bottle.color) {
+                case 'green': {
+                    let result = {...bottle, color: 'brown', usedGraphic: bottleBrownSmall}
+                    if (result.sizeIsNormal) result.usedGraphic = bottleBrownNormal
+                    return setBottle(result);
+                }
+                case 'brown' : {
+                    let result = {...bottle, color: 'white', usedGraphic: bottleWhiteSmall}
+                    if (result.sizeIsNormal) result.usedGraphic = bottleWhiteNormal
+                    return setBottle(result);
+                }
+                case 'white' : {
+                    let result = {...bottle, color: 'green', usedGraphic: bottleGreenSmall}
+                    if (result.sizeIsNormal) result.usedGraphic = bottleGreenNormal
+                    return setBottle(result);
+                }
+            }
+        }
+
+        const bottleSizeToggle = () => {
+            let result = {...bottle}
+
+            if (result.sizeIsNormal) {
+                result.sizeIsNormal = false
+                if (result.color === 'green')  result.usedGraphic = bottleGreenSmall
+                if (result.color === 'brown')  result.usedGraphic = bottleBrownSmall
+                if (result.color === 'white')  result.usedGraphic = bottleWhiteSmall
+                return setBottle(result);
+            } else {
+                result.sizeIsNormal = true
+                if (result.color === 'green')  result.usedGraphic = bottleGreenNormal
+                if (result.color === 'brown')  result.usedGraphic = bottleBrownNormal
+                if (result.color === 'white')  result.usedGraphic = bottleWhiteNormal
+                return setBottle(result);
+            }
+        }
+
+
+        return (
+
+            <div className={classes.flex}>
+                <img className={classes.bottleSize} src={bottle.usedGraphic}/>
+                <div>
+                    <h3>{headings.colorChange}</h3>
+                    <button onClick={bottleChangeHandlerLeft} className={classes.button}>previous</button>
+                    <button onClick={bottleColorChangeHandlerRight} className={classes.button}>next</button>
+                    <h3>SIZECHANGE</h3>
+                    <button onClick={bottleSizeToggle} className={classes.button}>CHANGE SIZE</button>
+                </div>
+            </div>
+        )
+    }
+;
 
 export default ConfigurationBottle;
