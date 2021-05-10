50f27ca722a4f2cddda9178f353ab8f4257008a9
HKrie <Harald.Kriebisch@gmx.de>
2021-05-10 09:19:26 +0200

    Add Panel navigation (unfinished)

--- src/components/Layout/Body/screens/ConfiguratorScreen/BierKonfigurator/Configuration/ConfigurationPanel/index.tsx
+++ src/components/Layout/Body/screens/ConfiguratorScreen/BierKonfigurator/Configuration/ConfigurationPanel/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ConfigurationSteps from "../ConfigurationSteps";
+import classes from "./ConfigurationPanel.module.css";
 
 const ConfigurationPanel = () => {
 
+    const [active, setActive] = useState(0);
+
+    const changeActive = (step: number) => {
+        setActive(step);
+    };
+
     return (
-        <section>
+        <section className={classes.mh_20}>
             <h2>Configuration-Panel</h2>
-            <ConfigurationSteps/>
+            <ConfigurationSteps active={active} changeActive={changeActive}/>
+
+            
+
+            <div className={classes.flex} style={{width:'100%', padding:'0px 4px'}}>
+                <div style={{width: '100%'}}>{(active !== 0) ? <button className={`${classes.btn} ${classes.btn_left}`}
+                                                                       onClick={() => changeActive(active - 1)}>Zurück</button> : null}</div>
+                <div style={{width: '100%'}}>{(active !== 3) ? <button className={`${classes.btn} ${classes.btn_right}`}
+                                                                       onClick={() => changeActive(active + 1)}>Weiter</button> : null}</div>
+            </div>
         </section>
     )
 };
--- src/components/Layout/Body/screens/ConfiguratorScreen/BierKonfigurator/Configuration/ConfigurationSteps/index.tsx
+++ src/components/Layout/Body/screens/ConfiguratorScreen/BierKonfigurator/Configuration/ConfigurationSteps/index.tsx
@@ -1,49 +1,120 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import classes from './configurationSteps.module.css';
 
-const ConfigurationSteps = () => {
 
-    const navPoints = ['Geschmack', 'Etikett', 'Größe'];
+const ConfigurationSteps = (props: any) => {
 
+    const [navPoints, setNavPoints] = useState([
+            {
+                name: 'etikett',
+                status: 1,
+                linkText: 'step-1',
+                hoverText: 'etikett',
+                showHover: false
+            },
+            {
+                name: 'Biersorte',
+                status: 0,
+                linkText: 'step-2',
+                hoverText: 'Biersorte',
+                showHover: false
+
+            },
+            {
+                name: 'Geschmack',
+                status: 0,
+                linkText: 'step-3',
+                hoverText: 'Geschmack',
+                showHover: false
+
+            },
+            {
+                name: 'Größe',
+                status: 0,
+                linkText: 'step-4',
+                hoverText: 'Größe',
+                showHover: false
+
+            },
+        ]
+    )
+
+
+    const changeActiveMarker = (nextActiveStep: number) => {
+        let temp: any = [...navPoints];
+        temp.forEach((point: any, i: number) => {
+            if (i <= nextActiveStep) {
+                temp[i].status = 1;
+            }
+        });
+        setNavPoints(temp);
+    }
+
+    const showHoverText = (element: number) => {
+        if (element !== props.active) {
+            let temp: any = [...navPoints];
+            temp[element].showHover = true;
+            setNavPoints(temp);
+        }
+
+    };
+
+    const hideHoverText = (element: number) => {
+        let temp: any = [...navPoints];
+        temp[element].showHover = false;
+        setNavPoints(temp);
+
+    }
+
+    /*
+    * if the user input in the panel forms isn't valid (incomplete) then make the dot red and if the user hasn't chosen anything make it grey.
+    * check using usestate
+    * add tooltip with title and subtext with (icon +) title for dots
+    * onmouseover effect
+* */
     return (
         <section>
-            <ul>
-                <div style={{display: 'flex', flexDirection: 'row', listStyle: 'none'}}>
-                    <li style={{width: 'available'}}>
-                        {/*add onmouseover*/}
-                        <div style={{display: "flex", flexDirection: "row"}}>
-                            <Link to='/beer-configurator/step-1' style={{marginLeft: "50%", float:'left'}}>
-                                <div className={`${classes.dot} ${classes.finished}`}/>
-                            </Link>
-                            <div
-                                 className={`${classes.connectionLine} ${classes.finished}`}/>
-                        </div>
-                        <div>Geschmacksrichtung</div>
-                    </li>
-
-                    <li>
-                        {/*add onmouseover*/}
-                        <div></div>
-                        <Link to='/beer-configurator/step-2'>
-                            <div className={`${classes.dot} ${classes.finished}`}/>
-                        </Link>
-                        <div></div>
-                    </li>
-                    <div className={`${classes.connectionLine} ${classes.unfinished}`}/>
-                    <li>
-                        {/*add onmouseover*/}
-                        <div></div>
-                        <Link to='/beer-configurator/step-3'>
-                            <div className={`${classes.dot} ${classes.unfinished}`}/>
-                        </Link>
-                        <div></div>
-                    </li>
-                    {/*add tooltip with title and subtext with (icon +) title for dots*/}
+            <ul style={{padding:0}}>{/*if active-> no hover and show name, inactive-> hover, but no name*/}
+                <div className={`${classes.flex} ${classes.noListStyle} ${classes.width100}`}>
+                    {navPoints.map((point, i) => {
+                        return (
+                            <li className={classes.flex_4} key={i}>
+                                <div
+                                    className={`${classes.hoverText} ${classes.textHeight}`}
+                                >{navPoints[i].showHover ? point.hoverText : ""}
+                                </div>
+                                <div className={classes.flex}>
+                                    <div
+                                        className={`${classes.connectionLine} ${point.status ? classes.finished : classes.unfinished} ${point.linkText === 'step-1' ? classes.transparent : ""}`}
+                                        style={{width: "100%"}}/>
+                                    <Link to={'/beer-configurator/' + point.linkText}
+                                          onMouseOver={() => {
+                                              showHoverText(i);
+                                          }}
+                                          onMouseLeave={() => {
+                                              hideHoverText(i);
+                                          }}
+                                          onClick={() => {
+                                              props.changeActive(i);
+                                              changeActiveMarker(i);
+                                              hideHoverText(i);
+                                          }}
+                                    >
+                                        <div
+                                            className={`${classes.dot} ${point.status ? classes.finished : classes.unfinished}`}/>
+                                    </Link>
+                                    {navPoints[i + 1] ? <div
+                                            className={`${classes.connectionLine2} ${navPoints[i + 1].status === 1 ? classes.finished : classes.unfinished}`}/> :
+                                        <div
+                                            className={`${classes.connectionLine2} ${classes.transparent}`}
+                                            style={{width: "100%"}}/>}
+                                </div>
+                                <div>{i === props.active ? point.name : null}</div>
+                            </li>
+                        )
+                    })}
                 </div>
-                {/*<li><Link to='/beer-configurator/step-2'>Etikett</Link></li>
-                    <li><Link to='/beer-configurator/step-1'>Geschmack</Link></li>
-                    <li><Link to='/beer-configurator/step-3'>Größe</Link></li>*/}
             </ul>
         </section>
     )
