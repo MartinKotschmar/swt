b9d7a996f305d9d9d9e91b614a1d6f342aaabb23
Martin Kotschmar <martin.kotschmar@gmail.com>
2021-05-22 10:50:55 +0200

    basic styles for input

--- src/components/Layout/Body/screens/ConfiguratorScreen/BierKonfigurator/Configuration/DeliveryDetails/index.tsx
+++ src/components/Layout/Body/screens/ConfiguratorScreen/BierKonfigurator/Configuration/DeliveryDetails/index.tsx
@@ -10,78 +10,148 @@ const DeliveryDetails = () => {
     setStatus(false);
     console.log(status);
   };
-  const submitClickHandler = () => {
-    //   if('checkImputFields') {
-    //       setCostomerDetails(
-    //       )
-    //   }
+  const submitClickHandler = (e: any) => {
+    e.preventDefault();
   };
 
   return (
     <div className={classes.wrapper}>
-      <form action="">
-        <div className={classes.personal_Details}>
-          <label html-for="title">Anrede</label>
-          <select name="title" id="title" onChange={optionChangeHandler}>
-            <option className={`active-${status}`} value="selection">
-              Bitte auswählen:
-            </option>
-            <option className={classes.radioOption} value="Herr">
-              Herr
-            </option>
-            <option className={classes.radioOption} value="Frau">
-              Frau
-            </option>
-          </select>
-        </div>
-        <label className="">
-          <span>Vorname</span>
-          <input type="text" minLength={2} />
-        </label>
-        <label className="">
-          <span>Nachname</span>
-          <input type="text" minLength={2} />
-        </label>
-        <div className={classes.adress}>
-          <div className={classes.adress_Child}>
-            <label className="">
-              <span>Straße</span>
-              <input type="text" minLength={3} />
-            </label>
+      <div className={classes.content}>
+        <form className={classes.form} action="">
+          <div className={classes.input_wrapper}>
+            <div className={classes.personal_Details_Wrapper}>
+              <div className={classes.personal_Details}>
+                <div className={classes.input_div_select}>
+                  <label className={classes.label}>Anrede</label>
+                  <select
+                    name="title"
+                    id="title"
+                    onChange={optionChangeHandler}
+                    required
+                  >
+                    <option className={`active-${status}`} value="selection">
+                      Bitte auswählen:
+                    </option>
+                    <option className={classes.radioOption} value="Herr">
+                      Herr
+                    </option>
+                    <option className={classes.radioOption} value="Frau">
+                      Frau
+                    </option>
+                  </select>
+                </div>
+                <div className={classes.input_div}>
+                  <label className={classes.label}>
+                    <span>Vorname</span>
+                  </label>
+                  <input
+                    className={`${classes.input} ${classes.input_long}`}
+                    type="text"
+                    minLength={2}
+                    pattern="[A-Za-z]"
+                    required
+                  />
+                </div>
+                <div className={classes.input_div}>
+                  <label className={classes.label}>
+                    <span>Nachname</span>
+                  </label>
+                  <input
+                    className={`${classes.input} ${classes.input_long}`}
+                    type="text"
+                    minLength={2}
+                    pattern="[A-Za-z]"
+                    required
+                  />
+                </div>
+                <div className={classes.input_div}>
+                  <label className={classes.label}>
+                    <span>E-Mail</span>
+                  </label>
+                  <input
+                    className={`${classes.input} ${classes.input_long}`}
+                    type="text"
+                    minLength={5}
+                    required
+                  />
+                </div>
+                <div className={classes.input_div}>
+                  <label className={classes.label}>
+                    <span>Telefonnummer</span>
+                  </label>
+                  <input
+                    className={`${classes.input} ${classes.input_long}`}
+                    type="number"
+                    minLength={5}
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className={classes.adress}>
+              <div className={classes.adress_Child}>
+                <label className={classes.label}>
+                  <span>Straße</span>
+                </label>
+                <input
+                  className={`${classes.input} ${classes.input_long}`}
+                  type="text"
+                  minLength={3}
+                  pattern="[A-Za-z0-9\s]"
+                />
+              </div>
+              <div className={classes.adress_Child}>
+                <label className={classes.label}>
+                  <span>Wohnort</span>
+                </label>
+                <input
+                  className={`${classes.input} ${classes.input_long}`}
+                  type="text"
+                  minLength={2}
+                  pattern="[A-Za-z]"
+                />
+              </div>
+              <div
+                className={`${classes.adress_Child} ${classes.adress_Child_short}`}
+              >
+                <label className={classes.label}>
+                  <span>Hausnummer</span>
+                </label>
+                <input
+                  className={`${classes.input} ${classes.input_short}`}
+                  type="number"
+                  min={1}
+                  pattern="[0-9]"
+                />
+              </div>
+              <div className={classes.adress_Child}>
+                <label className={classes.label}>
+                  <span>Postleitzahl</span>
+                </label>
+                <input
+                  className={`${classes.input} ${classes.input_short}`}
+                  type="number"
+                  min={1001}
+                  max={99998}
+                  minLength={5}
+                  maxLength={5}
+                  pattern="[0-9]"
+                />
+              </div>
+            </div>
           </div>
-          <div className={classes.adress_Child}>
-            <label className="">
-              <span>Hausnummer</span>
-              <input type="number" min={1} />
-            </label>
+          <div className={classes.button_wrapper}>
+            <button
+              type="submit"
+              className={classes.button}
+              onClick={submitClickHandler}
+            >
+              Lieferdetails bestätigen
+            </button>
           </div>
-          <div className={classes.adress_Child}>
-            <label className="">
-              <span>Postleitzahl</span>
-              <input
-                type="number"
-                min={1001}
-                max={99998}
-                minLength={5}
-                maxLength={5}
-              />
-            </label>
-          </div>
-          <div className={classes.adress_Child}>
-            <label className="">
-              <span>Wohnort</span>
-              <input type="text" minLength={2} />
-            </label>
-          </div>
-        </div>
-        <button
-          type="submit"
-          className={classes.button}
-          onClick={submitClickHandler}
-        >
-          Lieferdetails bestätigen
-        </button>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
