8917f27d27942d341207dea0a691b3aedd38f04f
David Hellner <danoobve@web.de>
2021-05-28 18:35:43 +0200

    Add Datenschutzerklärung

--- src/components/Layout/Body/screens/Datenschutzerklärung/index.tsx
+++ src/components/Layout/Body/screens/Datenschutzerklärung/index.tsx
@@ -1,8 +1,97 @@
 import React from 'react';
+import classes from "./datenschutzerklärung.module.css"
 
-const Datenschutzerklaerung = () =>{
-    return(
-        <h2>Datenschutzerklärung</h2>
+const Datenschutzerklaerung = () => {
+    return (
+        <div className={`${classes.wrapper}`}>
+            <h2>Datenschutzerklärung</h2>
+            <b>(Kurzform)</b><br/>
+
+            Datenschutz – ganz einfach<br/>
+
+            <p>Im Folgenden finden Sie einen kurzen, verständlichen Überblick über die Erhebung und Verarbeitung Ihrer
+                Daten. Detaillierte Erklärungen entnehmen Sie bitte unserer ausführlichen Datenschutzerklärung.</p>
+
+
+            <b>Diese Daten erfassen wir:</b><br/>
+
+            <p>Den Domain-Namen oder die IP- Adresse Ihres Computers, die Dateianfrage des Clients (Dateiname und URL),
+                den http-Antwort-Code und die Internetseite, von der aus Sie uns besuchen.
+                Wir verwenden Cookies. Sie können Ihren Browser so einstellen, dass diese Cookies nicht gespeichert oder
+                am Ende Ihrer Internetsitzung gelöscht werden.
+                Der Internetauftritt beinhaltet an verschiedenen Stellen spezifische Erfassungsformulare, die aus
+                Pflichtfeldern sowie aus freiwillig auszufüllenden Feldern unterschiedlichen Inhalts bestehen
+                können.
+                Innerhalb von PlugIns, die wir auf unserer Website nutzen.</p>
+
+
+            <b>So erheben wir Ihre Daten:</b><br/>
+
+            <p>Die Daten, die beim Besuch unserer Webseite anfallen, erheben wir automatisiert, gegebenenfalls durch den
+                Einsatz von Cookies. Ansonsten erheben wir die Daten nur auf Grund Ihrer Eingaben auf unserer Webseite,
+                in
+                den jeweiligen Erfassungsformularen. Wo immer dies der Fall ist, finden Sie direkt am Formular einen
+                Link zu
+                dem spezifischen Datenschutzhinweis</p><br/>
+
+
+            <b>Dafür nutzen wir Ihre Daten:</b><br/><br/>
+
+            Zur technischen Administration und zur Bereitstellung der Webseite.<br/>
+            Im Falle von speziellen Erfassungsformularen ist der genaue Zweck im jeweiligen Datenschutzhinweis
+            beschrieben.<br/>
+            Zur Weiterleitung an die Betreiber von Webdiensten, die wir auf der Website zur Verfügung stellen.<br/><br/>
+
+            <b>Außerdem nutzen wir Ihre Daten</b><br/>
+
+            <p>ausschließlich im gesetzlich vorgesehenen Rahmen<br/>
+                Zur Missbrauchserkennung und Störungsbeseitigung<br/>
+                Zur Auskunftserteilung gegenüber staatlichen Stellen<br/></p>
+
+            <b>So lange speichern wir Ihre Daten:</b><br/>
+
+            <p>Die Dauer der Datenspeicherung richtet sich in erster Linie nach der Zweckerfüllung. Das bedeutet, wir
+                speichern Ihre Daten genau so lange, wie wir diese benötigen um den Zweck der Datenerhebung zu erfüllen.
+                In
+                manchen Fällen liegen jedoch gesetzliche Aufbewahrungsfristen vor, die eine davon abweichende
+                Speicherdauer
+                vorsehen. In diesen Fällen speichern wir die Daten so lange, wie vom jeweiligen Gesetz vorgesehen.
+                Sollten
+                Daten in einem besonderen Zusammenhang nach einer anderen Frist gelöscht werden, so ist diese in der
+                ausführlichen Datenschutzerklärung angegeben.</p><br/>
+
+            <b>Diese Rechte haben Sie:</b><br/>
+
+            <p>Recht auf Auskunft (alle Auskünfte dieses Hinweises können Sie jederzeit auch erfragen)<br/>
+                Recht auf Löschung der Daten (es sei denn, es stehen Fristen dem entgegen)<br/>
+                Recht auf Vergessenwerden (sind Daten übermittelt worden, müssen wir den Empfänger zur Löschung
+                auffordern)<br/>
+                Recht auf Datenübertragbarkeit (wenn Sie es wünschen, müssen wir die Daten an einen Dritten
+                maschinenlesbar
+                übertragen)<br/>
+                Recht auf Berichtigung der Datenkategorie<br/>
+                Recht auf Widerspruch gegen die Verarbeitungstätigkeit<br/>
+                Recht auf Berichtigung falls die Daten fehlerhaft sind<br/>
+                Recht zur Beschwerde bei der Aufsichtsbehörde<br/>
+                Diese Datenschutzhinweise geben einen kompakten Überblick. Die vollständigen Informationen ergeben sich
+                aus
+                der ausführlichen Fassung.<br/></p>
+
+            <p>Zur Wahrung Ihrer Rechte und um die rechtlich einwandfreie Verarbeitung sicherzustellen, haben wir einen
+            externen Datenschutzbeauftragten benannt.</p>
+
+
+            <b>Dieses sind die Kontaktdaten:</b><br/><br/>
+
+            KreativBier GmbH<br/>
+            Kotschmar, Martin<br/>
+            Bierfeld 93<br/>
+            0815 Bierstadt<br/><br/>
+            <b>Kontaktaufnahme:</b><br/><br/>
+            Telefon: 0175/888 888 88<br/>
+            Fax: 0351/111 111 1<br/>
+            E-Mail: ichbinderchef@kreativbier.de<br/><br/>
+        </div>
     )
 };
 
