26865ead513b7b406c9019cf1943612c2ca6aab2
David Hellner <danoobve@web.de>
2021-06-03 20:09:34 +0200

    Add Footer-Styles

--- src/Wrapper.tsx
+++ src/Wrapper.tsx
@@ -1,31 +1,32 @@
 import MainHeader from "./components/Layout/Header/MainHeader";
-import { Link, Route, Switch } from "react-router-dom";
+import {Link, Route, Switch} from "react-router-dom";
 import ConfiguratorScreen from "./components/Layout/Body/screens/ConfiguratorScreen";
 import Datenschutzerklaerung from "./components/Layout/Body/screens/Datenschutzerklärung";
 import Impressum from "./components/Layout/Body/screens/Impressum";
 import AgeCheckModal from "./components/Layout/Body/overlays/AgeCheckModal";
-import React, { Fragment, useState } from "react";
+import React, {Fragment, useState} from "react";
+import Footer from "./components/Layout/Footer";
 
 const Wrapper = () => {
-  const [orders, setOrders] = useState([]);
+    const [orders, setOrders] = useState([]);
 
-  return (
-    <Fragment>
-      <MainHeader />
-      <Switch>
-        <Route path="/beer-configurator/:stepId">
-          <ConfiguratorScreen orders={orders} setOrders={setOrders} />
-        </Route>
-        <Route path="/data-protection" exact>
-          <Datenschutzerklaerung />
-        </Route>
-        <Route path="/imprint" exact>
-          <Impressum />
-        </Route>
-      </Switch>
-      <div className="PageWrapper">
-        <AgeCheckModal active={true} />
-        {/*<div className="header">
+    return (
+        <Fragment>
+            <MainHeader/>
+            <Switch>
+                <Route path="/beer-configurator/:stepId">
+                    <ConfiguratorScreen orders={orders} setOrders={setOrders}/>
+                </Route>
+                <Route path="/data-protection" exact>
+                    <Datenschutzerklaerung/>
+                </Route>
+                <Route path="/imprint" exact>
+                    <Impressum/>
+                </Route>
+            </Switch>
+            <div className="PageWrapper">
+                <AgeCheckModal active={true}/>
+                {/*<div className="header">
                     <div className="logo"/>
                     <div className="navigation">
                         <a href="#">Bierkonfigurator</a>
@@ -34,7 +35,7 @@ const Wrapper = () => {
                         <a href="#">Warenkorb</a>
                     </div>
                 </div>*/}
-        {/*<div className="body">
+                {/*<div className="body">
                     <div className="bierkonfigurator">
                         <h2>BierKonfigurator</h2>
                         <div className="bierkonfigurator-panel">
@@ -47,16 +48,10 @@ const Wrapper = () => {
 
                     </div>
                 </div>*/}
-        <div className="footer">
-          <div className="banner" />
-          <div className="nav-footer">
-            <Link to="/imprint">Impressum</Link>
-            <Link to="/data-protection">Datenschutzerklärung</Link>
-          </div>
-        </div>
-      </div>
-    </Fragment>
-  );
+               <Footer />
+            </div>
+        </Fragment>
+    );
 };
 
 export default Wrapper;
