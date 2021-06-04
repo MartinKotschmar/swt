dda5ff52ab8c5614a97d85f58545c7a92a157769
Martin Kotschmar <martin.kotschmar@gmail.com>
2021-06-04 08:14:58 +0200

    footer added

---
+++ src/components/Layout/Footer/index.tsx
@@ -0,0 +1,28 @@
+import { Link, NavLink } from "react-router-dom";
+import React from "react";
+import classes from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={`${classes.footer}`}>
+      <nav className={classes.navigation}>
+        <ul>
+          <li>
+            <NavLink to="/imprint">Impressum</NavLink>
+          </li>
+          <li>
+            <NavLink to="/data-protection">Datenschutzerklärung</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      {/*<div className="banner"/>*/}
+      {/*<div className="nav-footer">*/}
+      {/*    <Link to="/imprint">Impressum</Link>*/}
+      {/*    <Link to="/data-protection">Datenschutzerklärung</Link>*/}
+      {/*</div>*/}
+    </div>
+  );
+};
+
+export default Footer;
